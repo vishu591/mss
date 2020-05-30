3e59568ad4071c2893877c498aaaa25c304f0b0f
Kriti Goyal <goyalkriti8@gmail.com>
2020-05-30 18:30:50 +0530

    Spaces added

--- new_client.py
+++ new_client.py
@@ -1,22 +1,23 @@
 import socket
 
-s= socket.socket()
+s = socket.socket()
 host = "127.0.0.1"
 port = 9999
 
-s.connect((host,port))
+s.connect((host, port))
+
 
 def f_input():
     choice_msg = s.recv(1024)
     print("======== Received choice message from server: =========\n ", choice_msg.decode())
-    #User will provide the input as he wants to use the service
+    # User will provide the input as he wants to use the service
     choice_in = input ("Please choose the functionality you want to proceed with: ")
     s.send(choice_in.encode())
 
     choice_rec = s.recv(1024)       # According to selected choice, look for the service
     
     if choice_rec.decode() == '1':
-        echo_rcv= s.recv(1024).decode()
+        echo_rcv = s.recv(1024).decode()
         print(echo_rcv)
         client_echo()
 
@@ -26,8 +27,10 @@ def f_input():
     else:
         Break()
 
+
 def Break():
-    print ("Oops !!! you have entered the wrong input\nDisconnecting from server \a...")
+    print("Oops !!! you have entered the wrong input\nDisconnecting from server \a...")
+
 
 def client_echo():
     while True:
@@ -42,12 +45,15 @@ def client_echo():
             break
     s.close()
 
+
 def file_transfer():
-    print ("Currently the code is under development ")
-    pass                    #here FTS code will come
+    print("Currently the code is under development ")
+    pass                    # here FTS code will come
+
 
 def main():
     f_input()
+
 
 if __name__ == "__main__":
     main()
